0726258e0eab93560ea50802df82c9f2a594c16c
azank1 <azanhyder49@gmail.com>
2025-10-26 23:10:46 +0500

    chore: update market program ID to deployed address (Bk2pKQsXXvjPChX2G8AWgwoefnwRbTSirtHGnG8yUEdB)

---
+++ anomi-zk-prototype/rebuild-deploy.sh
@@ -0,0 +1,8 @@
+﻿#!/bin/sh
+cd /mnt/d/dev/zk2p/anomi-zk-prototype
+echo "Cleaning build..."
+cargo clean
+echo "Rebuilding programs..."
+anchor build
+echo "Deploying to devnet..."
+anchor deploy --provider.cluster devnet
